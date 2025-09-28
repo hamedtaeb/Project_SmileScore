88ddc17101c1faaade5eda27f9133a9d548b00a9
jeelfaldu7 <jeel.faldu7@gmail.com>
2025-09-28 09:09:14 -0700

    added python file for dashboard

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+# Import necessary libraries
+import re
+import os
+import random
+import sklearn
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import plotly.express as px
+from scipy import stats as st
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+from dash import Dash, html, dcc, callback, Output, Input, State
+import dash_bootstrap_components as dbc
